e2c33a28cb0e7eaa0298863138c007f489a6b82a
Дмитрий Комарницкий <dimko2003@yandex.ru>
2019-09-21 13:30:10 +0300

    All types Java

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte numByte = 73; //1 byte
+        short numShort = 542; //2 byte
+        int numInt = 6516451; //4 byte
+        long numLong = 546352784; //8 byte
+        float numFloat = 12.3213f; //4 byte
+        double numDouble = 2342.423421; //8 byte
+        char numChar = 'A'; //2 byte
+        boolean numBoolean = false; //1 byte
+
+    }
+}
