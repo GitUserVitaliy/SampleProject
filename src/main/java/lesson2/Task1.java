6af9a4de234f64e22993f86fa1b12d3218b91a5a
Черников Виталий <dimko2003@yandex.ru>
2019-09-22 10:51:35 +0300

    Task2.1

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -6,22 +6,19 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number: ");
-         int f_num = sc.nextInt();
+        int f_num = sc.nextInt();
         System.out.println("Enter second number: ");
-         int s_num = sc.nextInt();
+        int s_num = sc.nextInt();
         System.out.println("Enter third number: ");
-         int t_num = sc.nextInt();
-        if (f_num >= s_num && f_num >= t_num) {
-            System.out.println(f_num);
-            System.exit(0);
-        }
-        if (s_num >= f_num && s_num >= t_num) {
-            System.out.println(s_num);
-            System.exit(0);
-        }
-        if (t_num >= s_num && t_num >= f_num) {
-            System.out.println(f_num);
-            System.exit(0);
+        int t_num = sc.nextInt();
+        if (f_num <= s_num && f_num <= t_num) {
+            System.out.println("Mim-> " + f_num);
+        } else {
+            if (s_num <= f_num && s_num <= t_num) {
+                System.out.println("Mim-> " + s_num);
+            } else {
+                System.out.println("Mim-> " + t_num);
+            }
         }
     }
 }
