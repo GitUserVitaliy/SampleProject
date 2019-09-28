08d9f6aed0c07cb2cf0e7849ed8fcaa9636d349a
Черников Виталий <dimko2003@yandex.ru>
2019-09-28 13:11:41 +0300

    Exell

---
+++ src/main/java/lesson3/Task1.java
@@ -0,0 +1,33 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length: ");
+        float length = sc.nextFloat();
+        System.out.println("Enter E: ");
+        float e = sc.nextFloat();
+        float var = 0;
+        if(length > 0 && e >=0) {
+            while(true) {
+                System.out.println("Enter variable: ");
+                var = sc.nextFloat();
+                if (var != 0)
+                {
+                    if(var >= (length - e) && var <= (length + e)) {
+                        System.out.println("Yes");
+                    } else {
+                        System.out.println("No");
+                    }
+                } else {
+                    System.out.println("Exit");
+                    System.exit(0);
+                }
+            }
+        }
+    }
+}
+
+
