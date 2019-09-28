fc413a197cee20e53dffc66c1a8631622d8acebd
Черников Виталий <dimko2003@yandex.ru>
2019-09-28 12:53:16 +0300

    Temp++

--- src/main/java/lesson3/Temp.java
+++ src/main/java/lesson3/Temp.java
@@ -1,10 +1,12 @@
 package lesson3;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Temp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
         System.out.println(System.in);
         float _temp = sc.nextFloat();
         if(_temp >= 36.5 && _temp <= 36.7) {
