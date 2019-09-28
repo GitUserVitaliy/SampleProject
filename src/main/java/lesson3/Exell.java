6b6f3fa28f02c3d3ca93b25aac19a8bf5c1c5aa2
Черников Виталий <dimko2003@yandex.ru>
2019-09-28 13:13:06 +0300

    Rename

--- src/main/java/lesson3/Exell.java
+++ src/main/java/lesson3/Exell.java
@@ -2,7 +2,7 @@ package lesson3;
 
 import java.util.Scanner;
 
-public class Task1 {
+public class Exell {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter length: ");
