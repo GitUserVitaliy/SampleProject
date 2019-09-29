06b86a544f1cc9f5a5afe6d89768e3585a781d42
Черников Виталий <dimko2003@yandex.ru>
2019-09-29 11:33:30 +0300

    BadWords+++

--- src/main/java/lesson4/DeleteBadWords.java
+++ src/main/java/lesson4/DeleteBadWords.java
@@ -2,24 +2,8 @@ package lesson4;
 
 public class DeleteBadWords {
     public static String deleteBadWords(String str,String word) {
-        if(str.length() != 0 && word.length() != 0) {
-            int pos = str.indexOf(word);
-            if (str.indexOf(word) != -1) {
-                String result = "";
-                for (int i = 0; i < pos; i++) {
-                    result = result + str.charAt(i);
-                }
-                result = result + "***";
-                for (int i = pos + word.length(); i < str.length(); i++) {
-                    result = result + str.charAt(i);
-                }
-                System.out.println(str);
-                System.out.println(result);
-                return result;
-
-            } else {
-                 return str;
-            }
+        if(word.length() != 0) {
+            return str.replaceAll(word,"***");
         } else {
             return str;
         }
