17fc140bf1e67da7e6d4704484cce258d9780f29
Черников Виталий <dimko2003@yandex.ru>
2019-09-29 11:29:27 +0300

    BadWords++

--- src/main/java/lesson4/DeleteBadWords.java
+++ src/main/java/lesson4/DeleteBadWords.java
@@ -2,20 +2,24 @@ package lesson4;
 
 public class DeleteBadWords {
     public static String deleteBadWords(String str,String word) {
-        if(str.length() != 0)
-        {
+        if(str.length() != 0 && word.length() != 0) {
             int pos = str.indexOf(word);
-            String result = "";
-            for (int i = 0; i < pos; i++) {
-                result = result + str.charAt(i);
+            if (str.indexOf(word) != -1) {
+                String result = "";
+                for (int i = 0; i < pos; i++) {
+                    result = result + str.charAt(i);
+                }
+                result = result + "***";
+                for (int i = pos + word.length(); i < str.length(); i++) {
+                    result = result + str.charAt(i);
+                }
+                System.out.println(str);
+                System.out.println(result);
+                return result;
+
+            } else {
+                 return str;
             }
-            result = result + "***";
-            for (int i = pos + word.length(); i < str.length(); i++) {
-                result = result + str.charAt(i);
-            }
-            System.out.println(str);
-            System.out.println(result);
-            return result;
         } else {
             return str;
         }
--- src/test/java/test/lesson4/TestDel.java
+++ src/test/java/test/lesson4/TestDel.java
@@ -14,7 +14,25 @@ public class TestDel {
         String result = DeleteBadWords.deleteBadWords("", "love");
         assertEquals("", result);
     }
-
-
-
+    @Test
+    public void testCorrect() {
+        String result = DeleteBadWords.deleteBadWords("I love cats", "chair");
+        assertEquals("I love cats", result);
+    }
+    @Test
+    public void testStringNew() {
+        String result = DeleteBadWords.deleteBadWords("I love drugs, and cats", "drugs");
+        assertEquals("I love ***, and cats", result);
+    }
+    @Test
+    public void testTwice() {
+        String result = DeleteBadWords.deleteBadWords("I love drugs, and drugs", "drugs");
+        assertEquals("I love ***, and ***", result);
+    }
+    @Test
+    public void testWord() {
+        String result = DeleteBadWords.deleteBadWords("I love drugs, and drugs", "");
+        assertEquals("I love drugs, and drugs", result);
+    }
 }
+
