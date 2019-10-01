e324085f048ff69ad47bd8bd482648e1fbafbf4c
Виталий Черников <47694967+GitUserVitaliy@users.noreply.github.com>
2019-10-01 22:38:45 +0300

    homework2.2

--- src/main/java/homework2/Hello.java
+++ src/main/java/homework2/Hello.java
@@ -3,12 +3,8 @@ package homework2;
 public class Hello {
     public static boolean stringHello(String phrase) {
         String wordHello = "Здравствуйте";
-        if(phrase.indexOf(wordHello) != -1) {
-            if(phrase.indexOf(wordHello) == 0) {
-                return true;
-            } else {
-                return false;
-            }
+        if(phrase.indexOf(wordHello) == 0) {
+            return true;
         } else {
             return false;
         }
