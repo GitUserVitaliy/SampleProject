2cc8dd2cf8fb7edc16e520e926180712eb1ad651
Черников Виталий <dimko2003@yandex.ru>
2019-10-05 13:50:38 +0300

    Classes++

--- src/main/java/lesson5_analyze/KeyWordAnalyzer.java
+++ src/main/java/lesson5_analyze/KeyWordAnalyzer.java
@@ -1,4 +1,13 @@
 package lesson5_analyze;
 
 public class KeyWordAnalyzer {
+    protected String[] keyWords;
+    public boolean analyze(String text) {
+        for (int i = 0; i < keyWords.length; i++) {
+            if (text.indexOf(keyWords[i]) != -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
