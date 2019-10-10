ad852c330a56b58c771dc98dd4caf5b59d1b81a4
Виталий Черников <47694967+GitUserVitaliy@users.noreply.github.com>
2019-10-10 14:07:08 +0300

    Homework3

---
+++ src/main/java/homework3/Main.java
@@ -0,0 +1,35 @@
+package homework3;
+
+import javax.print.attribute.SetOfIntegerSyntax;
+import java.util.*;
+
+public class Main {
+    public static void deleteLong(List<String> list,int maxLength) {
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).length() > maxLength) {
+                list.remove(i);
+                i--;
+            }
+        }
+    }
+    public static Set<Integer> symmetricDifference(Set<Integer> setFirst, Set<Integer> setSecond) {
+        Set<Integer> setResult = new HashSet<>(setFirst);
+        setResult.addAll(setSecond);
+        Set<Integer> intersection= new HashSet<>(setFirst);
+        intersection.retainAll(setSecond);
+        setResult.removeAll(intersection);
+        return setResult;
+    }
+    public static Map<String,Integer> countInList(List<String> list) {
+        Map<String,Integer> map = new HashMap<>();
+        for (String current : list) {
+            if(map.containsKey(current)) {
+                map.put(current,map.get(current) + 1);
+            } else {
+                map.put(current,1);
+            }
+        }
+        return map;
+    }
+
+}
