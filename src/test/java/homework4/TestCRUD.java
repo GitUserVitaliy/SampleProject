b2074f6eadbb45c0d7c0e98d58098365a3205887
Виталий Черников <47694967+GitUserVitaliy@users.noreply.github.com>
2019-10-15 08:37:11 +0300

    <Поправки>

--- src/test/java/homework4/TestCRUD.java
+++ src/test/java/homework4/TestCRUD.java
@@ -8,21 +8,23 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCRUD {
-    UserService service = new UserService();
     @Test
     public void TestSignIn() {
-        assertEquals(service.auth("admin","admin"),true);
+        UserService TestSignIn = new UserService();
+        assertEquals(TestSignIn.auth("admin","admin"),true);
     }
     @Test
     public void TestSignIn_1() {
-        assertEquals(service.auth("AdmiN","aDmIn"),false);
+        UserService TestSignIn_1 = new UserService();
+        assertEquals(TestSignIn_1.auth("AdmiN","aDmIn"),false);
     }
     @Test
     public void TestSignUp() {
+        UserService TestSignUp = new UserService();
         AppUser user = new AppUser("root","toor","root toor",0);
-        service.addUser(user);
+        TestSignUp.addUser(user);
         boolean is_true = false;
-        List<AppUser> users = new LinkedList<>(service.getAllUsers());
+        List<AppUser> users = new LinkedList<>(TestSignUp.getAllUsers());
         for (int i = 0; i < users.size(); i++) {
             if(users.get(i).equals(user)) {
                 is_true = true;
@@ -32,21 +34,24 @@ public class TestCRUD {
     }
     @Test
     public void TestSignUp_1() {
+        UserService TestSignUp_1 = new UserService();
         AppUser user = new AppUser("root","toor","root toor",0);
-        assertEquals(service.addUser(user),true);
+        assertEquals(TestSignUp_1.addUser(user),true);
     }
     @Test
     public void TestSignUp_2() {
+        UserService TestSignUp_2 = new UserService();
         AppUser admin = new AppUser("admin","admin","admin",0);
-        assertEquals(service.addUser(admin),false);
+        assertEquals(TestSignUp_2.addUser(admin),false);
     }
     @Test
     public void TestRemoveUser(){
+        UserService TestRemoveUser = new UserService();
         AppUser user = new AppUser("root","toor","root toor",0);
-        service.addUser(user);
-        service.deleteUserByLogin(user.getLogin());
+        TestRemoveUser.addUser(user);
+        TestRemoveUser.deleteUserByLogin(user.getLogin());
         boolean is_true = false;
-        if(service.getAllUsers().get(0).equals(user)) {
+        if(TestRemoveUser.getAllUsers().get(0).equals(user)) {
             is_true = false;
         } else {
             is_true = true;
@@ -55,11 +60,13 @@ public class TestCRUD {
    }
     @Test
     public void TestRemoveUser_1(){
-        assertEquals(service.deleteUserByLogin("admin"),false);
+        UserService TestRemoveUser_1 = new UserService();
+        assertEquals(TestRemoveUser_1.deleteUserByLogin("admin"),false);
     }
     @Test
     public void TestRemoveUser_2(){
-        service.addUser(new AppUser("pro","orp","pro orp",0));
-        assertEquals(service.deleteUserByLogin("pro"),true);
+        UserService TestRemoveUser_2 = new UserService();
+        TestRemoveUser_2.addUser(new AppUser("pro","orp","pro orp",0));
+        assertEquals(TestRemoveUser_2.deleteUserByLogin("pro"),true);
     }
 }
