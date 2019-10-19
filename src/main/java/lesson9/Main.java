1df4393c76b3873d211617d6ab1257857bd0c6e8
Черников Виталий <dimko2003@yandex.ru>
2019-10-19 14:15:45 +0300

    <FileReader>

---
+++ src/main/java/lesson9/Main.java
@@ -0,0 +1,28 @@
+package lesson9;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("src\\main\\resources\\numbers.txt")) {
+            List<Integer> numbers = new LinkedList<>();
+            Scanner sc = new Scanner(reader);
+            while(sc.hasNextInt()) {
+                numbers.add(sc.nextInt());
+            }
+            float sum = 0f;
+            for (int i = 0; i < numbers.size(); i++) {
+                sum += numbers.get(i);
+            }
+            System.out.println(sum/numbers.size());
+
+        } catch (IOException exception) {
+            System.out.println("Ошибка при чтении из файла\n" + exception.getMessage());
+        }
+    }
+}
+
